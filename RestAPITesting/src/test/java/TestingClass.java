eeae8f2ab38cf8d83d2a7c3b1ad4e98171a2c54f
Manjunath Harakatti <manjunathharakatti@gmail.com>
2019-09-05 14:56:43 +0530

    Changes

--- RestAPITesting/src/test/java/TestingClass.java
+++ RestAPITesting/src/test/java/TestingClass.java
@@ -15,10 +15,10 @@ public class TestingClass {
 		//HashMap<String, String> param = new  HashMap<>();
 		//param.put("delay","0sec");
 		//bm.reportCheck("India",param,"manju");
-String a = SerenityRest.rest().given().baseUri("http://localhost:8080").queryParam("name", "gfutiyo").queryParam("mode", "hudson.model.FreeStyleProject").authentication().preemptive().basic("Manju_Harakatti", "11729cd24eb45b2a0a610fb95d9e325cb8").log().all().when().post("/view/all/createItem").headers().toString();
+SerenityRest.rest().given().baseUri("http://localhost:8080").queryParam("name", "gfutiyo").queryParam("mode", "hudson.model.FreeStyleProject").authentication().preemptive().basic("Manju_Harakatti", "11729cd24eb45b2a0a610fb95d9e325cb8").log().all().when().post("/view/all/createItem").then().statusCode(401);
 //String b = SerenityRest.rest().given().baseUri("http://localhost:8080").queryParam("delay", "0sec").authentication().preemptive().basic("Manju_Harakatti", "1160bf8395c499d561d933fa0ea73ecdc8").log().all().when().post("job/newProj/build").headers().toString();
 //Assert.assertTrue(true);
-    System.out.println("%%%%%%%%%%%%%%%%%%%%\n"+a);
+   // System.out.println("%%%%%%%%%%%%%%%%%%%%\n"+a);
 //Assert.assertTrue(a);
 	}
 
